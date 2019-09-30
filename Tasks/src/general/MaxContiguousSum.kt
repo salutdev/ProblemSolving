879937e3ed4bc6589cf338f542d3ac0da144d25d
salutdev <salutsirius@gmail.com>
2019-09-29 17:09:02 -0700

    Max contiguous sum with all negatives

--- Tasks/src/general/MaxContiguousSum.kt
+++ Tasks/src/general/MaxContiguousSum.kt
@@ -2,8 +2,14 @@ package general
 
 class MaxContiguousSum {
     fun find() {
-        val ar = arrayOf(-2, -3, -8, -5, -3, -2, -8, -5)
+        val ar = arrayOf(-2, -3, -8, 5, -3, 2, -3, 7)
 
+        var maxSum = getMaxSumWithAllNegatives(ar)
+
+        println(maxSum)
+    }
+
+    private fun getMaxSum(ar: Array<Int>): Int {
         var maxSum = Int.MIN_VALUE
         var curSum = 0
 
@@ -15,7 +21,25 @@ class MaxContiguousSum {
                 maxSum = curSum
             }
         }
+        return maxSum
+    }
 
-        println(maxSum)
+    private fun getMaxSumWithAllNegatives(ar: Array<Int>): Int {
+
+        var maxSum = Int.MIN_VALUE
+        var curSum = 0
+
+        for (e in ar) {
+            if (e > maxSum) maxSum = e
+            curSum += e
+            if (curSum < 0) {
+                curSum = 0
+            } else if (maxSum < curSum) {
+                maxSum = curSum
+            }
+
+        }
+
+        return  maxSum
     }
 }
