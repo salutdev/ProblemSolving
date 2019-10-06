0f14e3956facf0caba0da2586acc3fa64a7bd953
salutdev <salutsirius@gmail.com>
2019-10-05 21:24:51 -0700

    Insert node in BST

--- Tasks/src/Hello.kt
+++ Tasks/src/Hello.kt
@@ -2,10 +2,11 @@ import dynamic.LongestPalindromicSubsequece
 import dynamic.LongestPalindromicSubstring
 import general.Swap2Numbers
 import strings.KMP
+import trees.NodeInsertion
 import trees.NumberOfPathsWithSum
 import trees.SpecificSumExists
 
 fun main(args: Array<String>) {
 
-	KMP().findSubstring()
+	NodeInsertion().insert()
 }
---
+++ Tasks/src/trees/NodeInsertion.kt
@@ -0,0 +1,20 @@
+package trees
+
+class NodeInsertion {
+    fun insert() {
+        val root = Tree.getExampleTree1()
+        insertNode(root, 43)
+    }
+
+    private fun insertNode(root: TreeNode?, value: Int): TreeNode {
+        if (root == null) return TreeNode(value)
+
+        if (root.value > value) {
+            root.left = insertNode(root.left, value)
+        } else if (root.value < value) {
+            root.right = insertNode(root.right, value)
+        }
+
+        return root
+    }
+}
